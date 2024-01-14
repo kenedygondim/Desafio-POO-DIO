8af1ff3173323df23d0ef72ea576a845ab09923d
Kenedy Gondim <kenedyal478@gmail.com>
2024-01-14 11:38:39 -0300

    Desafio concluído

---
+++ Models/Nokia.cs
@@ -0,0 +1,14 @@
+namespace DesafioPOO.Models
+{
+    public class Nokia : Smartphone
+    {
+        public Nokia(string numero, string modelo, string imei, int memoria) : base(numero, modelo, imei, memoria)
+        {
+        }
+
+        public override void InstalarAplicativo(string nomeApp)
+        {
+            Console.WriteLine($"Aplicativo {nomeApp} instalado com sucesso no Nokia!");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using DesafioPOO.Models;
+
+
+Nokia nokia01 = new("11 965803584", "Lumia Xperia 13", "21568641-45664", 64);
+Console.WriteLine("Smarthphone Nokia registrado com sucesso.");
+nokia01.Ligar();
+nokia01.InstalarAplicativo("Whatsapp");
+
+Console.WriteLine("-----------------------------------------------------------");
+
+Iphone iphone01 = new("11 965804444", "Iphone 13", "41552658-48484", 128);
+Console.WriteLine("Smarthphone Iphone registrado com sucesso.");
+iphone01.ReceberLigacao();
+iphone01.InstalarAplicativo("Siri");
+
+
+ 
